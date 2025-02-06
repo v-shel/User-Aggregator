0fc7eecb28566b6d104e70b2c301f74bebd6eade
vshel <viktor.shelya@gmail.com>
2025-02-06 10:16:49 +0200

    Added second pg container for test

--- src/main/java/com/test/task/comparusmultidbread/model/entity/User.java
+++ src/main/java/com/test/task/comparusmultidbread/model/entity/User.java
@@ -3,7 +3,6 @@ package com.test.task.comparusmultidbread.model.entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
--- src/test/java/com/test/task/comparusmultidbread/UserAggregatorApplicationTests.java
+++ src/test/java/com/test/task/comparusmultidbread/UserAggregatorApplicationTests.java
@@ -6,19 +6,14 @@ import com.github.dockerjava.api.model.ExposedPort;
 import com.github.dockerjava.api.model.HostConfig;
 import com.github.dockerjava.api.model.PortBinding;
 import com.github.dockerjava.api.model.Ports;
-import com.test.task.comparusmultidbread.config.DataSourceProperties;
 import com.test.task.comparusmultidbread.controller.UserController;
 import com.test.task.comparusmultidbread.model.entity.User;
 import jakarta.annotation.PostConstruct;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.NoSuchElementException;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -30,47 +25,40 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 class UserAggregatorApplicationTests {
 
     @Container
-    private static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:latest")
+    private static final PostgreSQLContainer<?> POSTGRES1 = new PostgreSQLContainer<>("postgres:latest")
         .withDatabaseName("test_db1")
         .withUsername("user1")
         .withPassword("pass1")
-        .withInitScripts("db/scripts/init-db.sql")
-        .withExposedPorts(5432)
+        .withInitScripts("db/scripts/init-db1.sql")
         .withCreateContainerCmdModifier(cmd -> cmd.withHostConfig(
             new HostConfig().withPortBindings(new PortBinding(Ports.Binding.bindPort(55432), new ExposedPort(5432)))
         ));
 
+    @Container
+    private static final PostgreSQLContainer<?> POSTGRES2 = new PostgreSQLContainer<>("postgres:latest")
+        .withDatabaseName("test_db2")
+        .withUsername("user2")
+        .withPassword("pass2")
+        .withInitScripts("db/scripts/init-db2.sql")
+        .withCreateContainerCmdModifier(cmd -> cmd.withHostConfig(
+            new HostConfig().withPortBindings(new PortBinding(Ports.Binding.bindPort(55433), new ExposedPort(5432)))
+        ));
+
     @Autowired
     private UserController userController;
-    @Autowired
-    private Map<JdbcTemplate, DataSourceProperties> templates;
 
     @PostConstruct
     public void init() {
-        POSTGRES.start();
-
-        JdbcTemplate testDb2Template = templates.entrySet().stream()
-            .filter(entry -> entry.getValue().getUrl().contains("test_db2"))
-            .findFirst()
-            .map(Entry::getKey)
-            .orElseThrow(() -> new NoSuchElementException("No expected datasource."));
-
-        testDb2Template.execute("""
-            DROP TABLE IF EXISTS user_table;
-            CREATE TABLE user_table (
-                ldap_login varchar primary key not null,
-                name       varchar,
-                surname    varchar
-            );
-            INSERT INTO user_table (ldap_login, name, surname) VALUES ('ldap_login1', 'Jain', 'Melt');
-            INSERT INTO user_table (ldap_login, name, surname) VALUES ('ldap_login2', 'Keith', 'Brooks');
-        """);
+        POSTGRES1.start();
+        POSTGRES2.start();
     }
 
     @Test
     void testGetUsers() {
         List<User> users = userController.getUsers();
+
         assertThat(users).isNotNull().size().isEqualTo(4);
+
         assertThat(users)
             .extracting(User::getId)
             .allMatch(StringUtils::isNotBlank);
